6875bdb3d82a10dd5b6cbfdb8c8c5cfcb2294a37
DanieliusNev <danieliusneverdauskas@gmail.com>
2023-05-16 10:31:01 +0200

    hjg

---
+++ Shared/Calendar/CalendarEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Calendar;
+
+public class CalendarEvent
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
