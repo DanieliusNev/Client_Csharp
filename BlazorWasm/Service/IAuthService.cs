5d9162e02fd8d5bb099b41e201c68b5e01969a0d
DanieliusNev <danieliusneverdauskas@gmail.com>
2023-05-17 17:42:22 +0200

    update with login

---
+++ BlazorWasm/Service/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Shared.Models;
+
+namespace BlazorWasm.Services;
+
+public interface IAuthService
+{
+    /*public Task LoginAsync(string username, string password);*/
+    public Task LogoutAsync();
+    public Task RegisterAsync(User user);
+    public Task<ClaimsPrincipal> GetAuthAsync();
+    /*public Task PostCreate(Post post);
+
+    Task<UserBasicDto> GetUserByName(string username);*/
+    public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }
+}
---
+++ Shared/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Shared.Models;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    /*[JsonIgnore]
+    public ICollection<Post> Posts { get; set; }*/
+}
